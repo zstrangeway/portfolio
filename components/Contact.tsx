14e12e0bb659a49e9af6e4ad5a36384c8d22fb9f
Zachary Strangeway <zachary.strangeway@tasconline.com>
2020-10-06 22:25:53 -0500

    contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,8 +1,16 @@
+import styles from "../styles/Contact.module.scss";
+
 const Contact = (): JSX.Element => {
   return (
-    <div>
-      <h2>Contact</h2>
-    </div>
+    <section className={styles.section}>
+      <h2 className={styles.section_header}>Contact</h2>
+      <div className={styles.form}>
+        <input className={styles.input} type="text" placeholder="Name" />
+        <input className={styles.input} type="text" placeholder="Email" />
+        <input className={styles.input} type="text" placeholder="Subject" />
+        <textarea className={styles.input} placeholder="Message"></textarea>
+      </div>
+    </section>
   );
 };
 
