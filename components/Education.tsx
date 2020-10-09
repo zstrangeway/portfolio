023b37cdbe4dbdb8b5edcf111705258e635f642c
Zachary Strangeway <zachary.strangeway@tasconline.com>
2020-10-09 01:25:48 -0500

    update education

--- components/Education.tsx
+++ components/Education.tsx
@@ -2,11 +2,13 @@ import styles from "../styles/Education.module.scss";
 
 const Education = (): JSX.Element => {
   return (
-    <section className={styles.section}>
-      <h2 className={styles.section_header}>Education</h2>
-      <h3>Herzing University</h3>
-      <p>B.S. Software Development</p>
-      <p>2011 - 2014</p>
+    <section className={styles.education}>
+      <div className={styles.education_container}>
+        <h2 className={styles.education_title}>Education</h2>
+        <h3 className={styles.education_school}>Herzing University</h3>
+        <p className={styles.education_degree}>B.S. Software Development</p>
+        <p className={styles.education_dates}>2011 - 2014</p>
+      </div>
     </section>
   );
 };
