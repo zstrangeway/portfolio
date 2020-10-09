ebe1342c996b4e5e5dd1b6c402f53085f8ec556d
Zachary Strangeway <zachary.strangeway@tasconline.com>
2020-10-09 01:02:36 -0500

    update hero to use base container styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,17 @@
 import styles from "../styles/Hero.module.scss";
-import Linkedin from "./Icons/Linkedin";
-import Github from "./Icons/Github";
 
 const Hero = (): JSX.Element => {
-  const svgSize = "48px";
   return (
     <section className={styles.hero}>
-      <div className={styles.hero_title_wrapper}>
-        <h1 className={styles.hero_title}>
-          Zachary
-          <br />
-          Strangeway
-        </h1>
-        <h2 className={styles.hero_sub_title}>Senior Software Engineer</h2>
+      <div className={styles.hero_container}>
+        <div className={styles.hero_title_wrapper}>
+          <h1 className={styles.hero_title}>
+            Zachary
+            <br />
+            Strangeway
+          </h1>
+          <h2 className={styles.hero_sub_title}>Senior Software Engineer</h2>
+        </div>
       </div>
     </section>
   );
